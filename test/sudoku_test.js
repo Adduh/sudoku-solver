a9e40447adb610a11e6ebc9f51174f32dc765bc9
Artur Taube <artur.taube@sophos.com>
2014-08-29 13:07:17 +0200

    add combining solving strategy

--- test/sudoku_test.js
+++ test/sudoku_test.js
@@ -288,4 +288,23 @@ describe('sudoko.js', function() {
                 [2, u, 9,  4, 5, 7,  u, u, u]]);
         });
     });
+
+    describe('solve()', function() {
+        it('solves a hard sudoku', function() {
+            sudokuSolver = new SudokuSolver(hard);
+            sudokuSolver.solve();
+            assert.deepEqual(sudokuSolver.getField(),  [
+                [ 9, 8, 1,  7, 2, 4,  3, 6, 5 ],
+                [ 3, 2, 4,  6, 1, 5,  8, 7, 9 ],
+                [ 7, 6, 5,  9, 8, 3,  1, 4, 2 ],
+
+                [ 1, 9, 7,  8, 3, 6,  2, 5, 4 ],
+                [ 6, 4, 2,  5, 7, 1,  9, 3, 8 ],
+                [ 8, 5, 3,  2, 4, 9,  7, 1, 6 ],
+
+                [ 4, 7, 6,  3, 9, 8,  5, 2, 1 ],
+                [ 5, 3, 8,  1, 6, 2,  4, 9, 7 ],
+                [ 2, 1, 9,  4, 5, 7,  6, 8, 3 ]]);
+        });
+    });
 });
